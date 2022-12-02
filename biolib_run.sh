e723fc242e2b1694c7c0ab72d43a919f33339bb6
makarosca <93660259+makarosca@users.noreply.github.com>
2022-12-02 13:52:50 +0100

    Update biolib_run.sh

--- biolib_run.sh
+++ biolib_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/root/miniconda3/envs/rfdesign-cuda/bin/python3 RFDesign/hallucination/hallucinate.py $@
+/root/miniconda3/envs/rfdesign-cuda/bin/python3 RFDesign/hallucination/hallucinate.py $@ --out /output/design

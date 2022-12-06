e190e8f9e2d77c8399307d68a649d906549d4281
laura-biolib <laura@biolib.com>
2022-12-06 19:51:44 +0000

    Working AF2 monomer metrics

--- biolib_run.sh
+++ biolib_run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 mkdir /output
+
+# Run hallucination
 /root/miniconda3/envs/rfdesign-cuda/bin/python3 RFDesign/hallucination/hallucinate.py $@ --out /output/design
+
+# Run rosetta relaxation
+RFDesign/scripts/trfold_relax.sh /output/
+
+# Run AF2 metrics
+/root/miniconda3/envs/rfdesign-cuda/bin/python3 RFDesign/scripts/af2_metrics.py /output/trf_relax
+
+# Run PyRosetta metrics
+/root/miniconda3/envs/rfdesign-cuda/bin/python3 RFDesign/scripts/pyrosetta_metrics.py /output/trf_relax
--- scripts/trfold_relax.sh
+++ scripts/trfold_relax.sh
@@ -11,9 +11,9 @@
 # will relax all pdb files in folders, using restraints derived from matching
 # npz files, and output relaxed structures into a subfolder called trf_relax/.
 #
-
+​
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+​
 OPTIND=1
 roll="--roll"
 while getopts ":n" opt; do
@@ -22,13 +22,13 @@ while getopts ":n" opt; do
     esac
 done
 shift $((OPTIND-1))
-
+​
 if [ -z "$2" ]; then
     queue=short
 else
     queue=$2
 fi
-
+​
 outdir=$1/trf_relax
 mkdir -p $outdir
 task_file=`basename $1`.fold.list
@@ -36,20 +36,11 @@ for PDB in $1/*.pdb; do
   f=`basename $PDB .pdb`
   NPZ=`dirname $PDB`/$f.npz
   if [ ! -f $outdir/$f.pdb ]; then
-    echo "$DIR/RosettaTR/trfold_relax.py $roll -sg 7,3 $NPZ $PDB $outdir/$f.pdb"
+    echo "Relaxing $PDB"
+    /root/miniconda3/envs/rfdesign-cuda/bin/python3 $DIR/RosettaTR/trfold_relax.py $roll -sg 7,3 $NPZ $PDB $outdir/$f.pdb
   fi
-done > $task_file
-
-count=$(cat $task_file | wc -l)
-if [ "$count" -gt 0 ]; then
-    echo "Relaxing $count designs..."
-    sbatch -a 1-$(cat $task_file | wc -l) -J fold.`basename $1` -c 1 -p $queue --mem=4g \
-           -o /dev/null -e /dev/null \
-           --wrap="eval \`sed -n \${SLURM_ARRAY_TASK_ID}p $task_file\`"
-else
-    echo "No designs need relaxing. To refold, delete or move existing .pdb files."
-fi
-
+done
+​
 cd $outdir
 ct=$(find ../ -maxdepth 1 -name '*.trb' | wc -l)
 if [ "$ct" -gt 0 ]; then
